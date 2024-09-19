d92cc3e41d4b4ca729b86596abc24a7d80a7113b
tmalik258 <talhamalik25.tm@gmail.com>
2024-09-19 16:43:34 +0500

    added close-btn, fixed animations

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 $.when($.ready).then(function () {
 	// Document is ready.
 	$.each($(".gallery-grid .gallery-item"), function (index, element) {
-		$(element).click(function (e) { 
+		const img = element.children[0];
+		const galleryItemLarge = element.children[1];
+		const closeBtn = galleryItemLarge.children[0];
+	
+		$(img).click(function (e) {
 			e.preventDefault();
-			console.log(e.target.nextElementSibling)
-			const sibling = e.target.nextElementSibling;
-			$(sibling).addClass('active');
+			$(galleryItemLarge).removeClass('hide');
+			$(galleryItemLarge).addClass('show');
+		});
+	
+		$(closeBtn).click(function (e) {
+			e.preventDefault();
+			$(galleryItemLarge).removeClass('show');
+			$(galleryItemLarge).addClass('hide');
 		});
 	});
+	
+	
 });
